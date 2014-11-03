138e21e37dd7b7fffc2eeffd56e594c1eebd729b
xcxooxl <dudinirgad@Gmail.com>
2014-11-04 00:14:44 +0200

    Revert "Revert "Reverse Support Added""
    This reverts commit 37d9835fc4872b7fc0c5b314eff1e5489655b2d4.

---
+++ Reverse Support/Reverse Support/Reverse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Reverse_Support
+{
+    internal class Reverse_Support
+    {
+        public static Obj_AI_Hero Player;
+        public Menu Config;
+
+    
+
+    public Reverse_Support()
+    {
+        CustomEvents.Game.OnGameLoad += OnGameLoad;
+    }
+
+        private void OnGameLoad(EventArgs args)
+        {
+            Game.OnGameUpdate += OnGameUpdate;
+            Player = ObjectManager.Player;
+            Config = new Menu("Hecarim Reverse Support", "Reverse", true);
+            Config.AddToMainMenu();
+            Config.AddSubMenu(new Menu("Reverse Support Settings", "Reverse"));
+            Config.SubMenu("Reverse").AddItem(new MenuItem("Toggle", "Activate?").SetValue(false));
+
+            Config.SubMenu("Reverse").AddItem(new MenuItem("Slider", "Minion Health").SetValue(new Slider(100, 50, 500)));
+            
+            Game.PrintChat("<font color='#F7A100'>Reverse Support Hecarim by XcxooxL Loaded 1.0 .</font>");
+
+
+        }
+
+        private void OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("Toggle").GetValue<bool>())
+            {
+               var getm = MinionManager.GetMinions(Player.Position, 500, MinionTypes.All, MinionTeam.Ally, MinionOrderTypes.Health);
+               if (getm.Count == 0) return;
+                foreach (var minion in getm)
+                {
+                    if (minion.Health <= Config.Item("Slider").GetValue<int>())
+                    {
+                        Player.IssueOrder(GameObjectOrder.MoveTo, minion.Position);
+                    }
+                }
+            }
+        }
+    }
+
+    }
